a1d1cc11e1a1d32ecc5abf65611279250bcb285b
Dani de Waal <danidewaal@aol.com>
2017-11-21 18:20:42 -0500

    find factors

--- git.js
+++ git.js
@@ -19,3 +19,43 @@ function mindPsAndQs(str) {
 }
 
 console.log(mindPsAndQs('PPPXQPPPQ'));
+
+// Write a function commonFactors(num1, num2) that returns an array that contains
+// the common factors between both numbers. A factor is a number that divides
+// another number with no remainder.
+//
+// Examples:
+//
+// commonFactors(12, 50); // => [ 1, 2 ]
+// commonFactors(6, 24); // => [ 1, 2, 3, 6 ]
+// commonFactors(11, 22); // => [ 1, 11 ]
+// commonFactors(45, 60); // => [ 1, 3, 5, 15 ]
+// assertEqualArray([1,2,3,6], commonFactors(6, 24), 'returns common factors');
+
+
+function commonFactors(num1, num2) {
+   var common = [];
+   var factors1 = findFactors(num1);
+   var factors2 = findFactors(num2);
+
+   for (var i = 0; i < factors1.length; i++) {
+      var num = factors1[i];
+      if (factors2.indexOf(num) > -1) {
+         common.push(num);
+      }
+   }
+   return common;
+}
+
+function findFactors(num) {
+   var factors = [];
+   for (var i = 1; i <= num; i++) {
+      if (num % i === 0) {
+         factors.push(i);
+      }
+   }
+   return factors;
+}
+
+console.log(findFactors(25));
+console.log(commonFactors(6, 24));
